1bb097dcdd3a81c03bfcffae699d90be12ecd24e
jasba24 <johanbuitrago342@gmail.com>
2021-04-08 13:27:43 -0500

    add the images and the last styles

--- src/container/App.jsx
+++ src/container/App.jsx
@@ -1,16 +1,18 @@
 import React from "react"
 import Header from "../components/Header"
-import "../assets/styles/App.scss"
 import Search from "../components/Search"
 import Categories from "../components/Categories"
 import Carousel from "../components/Carousel"
 import CarouselItem from "../components/CarouselItem"
+import Footer from "../components/Footer"
+
+import "../assets/styles/App.scss"
 
 const App = () => (
 	<div className="App">
 		<Header />
 		<Search />
-		<Categories>
+		<Categories title="Mi lista">
 			<Carousel>
 				<CarouselItem />
 				<CarouselItem />
@@ -18,6 +20,25 @@ const App = () => (
 				<CarouselItem />
 			</Carousel>
 		</Categories>
+
+		<Categories title="Tendencias">
+			<Carousel>
+				<CarouselItem />
+				<CarouselItem />
+				<CarouselItem />
+				<CarouselItem />
+			</Carousel>
+		</Categories>
+
+		<Categories title="Originales de Platzi Video">
+			<Carousel>
+				<CarouselItem />
+				<CarouselItem />
+				<CarouselItem />
+				<CarouselItem />
+			</Carousel>
+		</Categories>
+		<Footer />
 	</div>
 )
 
